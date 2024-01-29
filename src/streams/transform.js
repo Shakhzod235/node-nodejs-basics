24bead278a72f41cc498fc48b76463a41241e1a1
Shakhzod235 <shakhzodikramov235@gmail.com>
2024-01-30 01:42:08 +0500

    feat: add solutions to streams tasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { stdin, stdout } from "process";
+import { Transform } from "stream";
+
+const reverse = new Transform({
+  transform(chunk, encoding, callback) {
+    callback(null, chunk.toString().split("").reverse().join(""));
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  try {
+    stdin.pipe(reverse).pipe(stdout);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await transform();
