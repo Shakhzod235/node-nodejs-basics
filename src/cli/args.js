0d5166a68716b931cff3e2396ae1b64e9fee2b73
Shakhzod235 <shakhzodikramov235@gmail.com>
2024-01-30 00:07:38 +0500

    feat: add solutions to cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const res = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const propName = args[i].replace("--", "");
+    const propValue = args[i + 1];
+    res.push(`${propName} ${propValue}`);
+  }
+
+  console.log(res.join(", "));
 };
 
 parseArgs();
