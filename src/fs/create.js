d3efb9df1681afde8f951b71b849b2d23da5d790
Shakhzod235 <shakhzodikramov235@gmail.com>
2025-04-29 03:32:53 +0500

    feat: add fs solutions

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import { appendFile } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    appendFile(`${__dirname}/files/fresh.txt`, 'I am fresh and young', 'utf-8', (err) => {
+        if (err) throw Error('FS operation failed')
+    });
+    console.log('File created!');
 };
 
 await create();
